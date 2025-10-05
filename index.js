3e19a307a9cc0fd7bf2bfd604167060dd4a20f12
Steven <s.coverdale@hotmail.com>
2025-10-05 19:12:21 -0400

    updated index

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: 'comp3123_assigment1',
+}).then(() => console.log('✅ MongoDB connected'))
+    .catch(err => console.error('❌ MongoDB error:', err));
+
+// Routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/emp', employeeRoutes);
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('<h1>COMP3123 Assignment 1 API is running locally</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
