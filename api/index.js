192697503b87931f41e98008fc3e3af8ee9e5d17
Steven <s.coverdale@hotmail.com>
2025-10-05 19:30:18 -0400

    Fixing vercel

--- api/index.js
+++ api/index.js
@@ -16,3 +16,8 @@ app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
 module.exports = app;
+
+app.use((err, req, res, next) => {
+    console.error('❌ Server error:', err);
+    res.status(500).json({ status: false, message: 'Internal Server Error' });
+});
