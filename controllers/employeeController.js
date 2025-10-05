ff4c078caa8b82077198f3c9ddf2a4c48f08fafc
Steven <s.coverdale@hotmail.com>
2025-10-05 19:10:37 -0400

    Updated Controllers

---
+++ controllers/employeeController.js
@@ -0,0 +1,26 @@
+const Employee = require('../models/Employee');
+
+exports.getAll = async (req, res) => {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+};
+
+exports.create = async (req, res) => {
+    const emp = await Employee.create(req.body);
+    res.status(201).json({ message: 'Employee created successfully.', employee_id: emp._id });
+};
+
+exports.getOne = async (req, res) => {
+    const emp = await Employee.findById(req.params.eid);
+    res.status(200).json(emp);
+};
+
+exports.update = async (req, res) => {
+    await Employee.findByIdAndUpdate(req.params.eid, req.body);
+    res.status(200).json({ message: 'Employee details updated successfully.' });
+};
+
+exports.delete = async (req, res) => {
+    await Employee.findByIdAndDelete(req.query.eid);
+    res.status(204).send();
+};
---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, email, password: hashed });
+        res.status(201).json({ message: 'User created successfully.', user_id: user._id });
+    } catch (err) {
+        res.status(400).json({ status: false, message: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = await User.findOne({ $or: [{ email }, { username }] });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ status: false, message: 'Invalid Username and password' });
+        }
+        res.status(200).json({ message: 'Login successful.' });
+    } catch (err) {
+        res.status(400).json({ status: false, message: err.message });
+    }
+};
